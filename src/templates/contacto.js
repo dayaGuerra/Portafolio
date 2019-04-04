033de730deb61728a21871494b10b5f5322de8e4
dayaGuerra <dayamandalaman@gmail.com>
2019-04-03 19:19:25 -0500

    Cambios poratafolio

--- src/templates/contacto.js
+++ src/templates/contacto.js
@@ -4,7 +4,9 @@
         divElement.setAttribute('id', 'proyectos');
         const tempForMe = `
         <section id="contact">
-        <div class=" container margin">
+        <div class="container margin">
+        <div class="card border-0 shadow my-5">
+        <div class="p-5">
         <div>
           <h2 class="font-weight-light"><strong>Contactame</strong></h2>
               <h1 class="font-weight-light margin-text">D'yanara Guerra riega</h1>
@@ -13,6 +15,9 @@
             <p class="lead margin-text"> Front-End Developer con intereses en desarrollar productos web de mayor calidad, manteniendo las buenas practicas  en el desarrollo con código limpio y reutilizable, aprender mas sobre  Javascript y la librería  React.</p>
             <p class="lead">Con  habilidades para enfrentar nuevos desafíos y la determinación de mejorar continuamente como profesional y como ser humano en cada tarea encomendada.
             Investigando y resolviendo problemas.</p>
+            </div>
+            </div>
+            </div>
             </div>
       </section>
     
